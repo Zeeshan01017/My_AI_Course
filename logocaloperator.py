ffe0603422c5c4bb15d33ed5446281c2344aa5ff
Muhammad Zeeshan <mzeeshan01017@gmail.com>
2025-07-11 08:04:56 -0700

    my code

---
+++ logocaloperator.py
@@ -0,0 +1,5 @@
+a = 5
+b = 6
+
+print((a > 2) and (b >= 6))    # True
+print((a > 2) or (b >= 6))
---
+++ operations.py
@@ -0,0 +1,15 @@
+a = 100
+b = 200
+print ('Sum: ', a + b)  
+
+print ('Subtraction: ', a - b)   
+
+print ('Multiplication: ', a * b)  
+
+print ('Division: ', a / b) 
+
+print ('Floor Division: ', a // b) 
+
+print ('Modulo: ', a % b)  #used for reminder values
+
+print ('Power: ', a ** b) 
